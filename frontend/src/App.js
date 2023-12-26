d0c355fa24d9ff901cf08c7a81da1cbc826a8f2e
shubham <shubham@gmail.com>
2023-12-26 00:25:06 +0000

    frontend backend and ngnix config files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      {/* Your content goes here */}
+    </div>
+  );
+}
+
+export default App;
+
